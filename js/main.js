1ccd035fa5f34a2b83615e55bdcf752443e85e55
EdMSL <eduard2011rus@gmail.com>
2018-08-27 22:28:02 +0700

    #29 пробую реализовать добавление товара в корзину

--- js/main.js
+++ js/main.js
@@ -3,12 +3,32 @@ var image = 'img/pin_icon.png';
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 59.938743, lng: 30.323058},
-          zoom: 18
+          zoom: 17
         }),
 
         marker = new google.maps.Marker({
-          position: {lat: 59.938743, lng: 30.323419},
+          position: {lat: 59.938743, lng: 30.323058},
           map: map,
           icon: image
         });
-      }
+			}
+function setId(){
+	var good = document.getElementsByClassName('goods_item');
+	
+	for(var i = 0; i < good.length; i++){
+		good[i].id = 'goods_item' + (i + 1);
+	}
+	var byBtn = document.getElementsByClassName('goods_view_btn');
+	for(var i = 0; i < byBtn.length; i++){
+		byBtn[i].id = 'goods_view_btn' + (i + 1);
+		byBtn[i].addEventListener('click', addGoodToCart);
+	}
+	console.log(good[0].p);
+}			
+function addGoodToCart(){
+	var goodName = document.getElementsByClassName('standart_link');
+	var goodPrice = document.getElementsByClassName('goods_price');
+	var basket = document.getElementsByClassName('pop-up_basket');
+
+}
+setId();
